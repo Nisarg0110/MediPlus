8cb084f47c716bf515278177be3d10904205923b
nisarg kadliya <nisargkadliya@gmail.com>
2024-08-09 23:16:16 +0530

    commit

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,36 @@
+import React,{useEffect} from 'react';
+import axios from 'axios';
+
+
+const HomePage = () => {
+    const getUserData=async ()=>{
+        try {
+            const res=await axios({
+                method:"post",
+                baseURL:"http://localhost:8000/api/v1",
+                url:`/user/getUserData`,
+                headers:{
+                    Authorization:"Bearer "+localStorage.getItem('token'),
+                  }
+              });
+            // const res=await axios.post("/api/v1/user/getUserData",{},{headers:{
+            //         Authorization:"Bearer "+localStorage.getItem('token'),
+            //       },})
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getUserData();
+    },[]);
+    return (
+        <div>
+            <h1>
+                HomePage
+            </h1>
+        </div>
+    )
+}
+
+export default HomePage;
